651c801b728f163890f4b572534854aace2cac62
Sergio Alberto Luis Cano <serluis@ucm.es>
2020-11-17 12:48:35 +0100

    punto 35 hecho

--- ProyectosOGREvc15x86/IG2App/Simbad.cpp
+++ ProyectosOGREvc15x86/IG2App/Simbad.cpp
@@ -1,1 +1,76 @@
 #include "Simbad.h"
+
+bool Simbad::keyPressed(const OgreBites::KeyboardEvent& evento)
+{
+    
+    if (evento.keysym.sym == SDLK_c) {
+       
+        StopAnim();     //detenemos todas las animaciones iniciales
+        ent->detachObjectFromBone(sword1_);
+        ent->detachObjectFromBone(sword2_);
+        ent->attachObjectToBone("Sheath.L", sword1_);
+        ent->attachObjectToBone("Sheath.R", sword2_);
+
+        anim = !anim;
+        if (anim) {     // Baile es la animacion base
+            animationsSinbad_.at(0)->setEnabled(true);
+            animationsSinbad_.at(0)->setLoop(true);
+        }
+        else {
+            animationsSinbad_.at(9)->setEnabled(true);
+            animationsSinbad_.at(9)->setLoop(true);
+            // Equilibrio manos-torso
+            animationsSinbad_.at(10)->setEnabled(true);
+            animationsSinbad_.at(10)->setLoop(true);
+        }
+    }
+    if (evento.keysym.sym == SDLK_e) {
+        StopAnim();
+        ent->detachObjectFromBone(sword1_);
+        ent->detachObjectFromBone(sword2_);
+        ent->attachObjectToBone("Sheath.L", sword1_);
+        ent->attachObjectToBone("Sheath.R", sword2_);
+
+        anim = !anim;
+        
+        if (anim) {
+            ent->detachObjectFromBone(sword1_);
+            ent->attachObjectToBone("Handle.L", sword1_);
+            animationsSinbad_.at(1)->setEnabled(true);
+        }
+        else {
+            ent->detachObjectFromBone(sword2_);
+            ent->attachObjectToBone("Handle.R", sword2_);
+            animationsSinbad_.at(1)->setEnabled(true);
+        }
+        animationsSinbad_.at(4)->setEnabled(true);
+        animationsSinbad_.at(4)->setLoop(true);
+        animationsSinbad_.at(5)->setEnabled(true);
+        animationsSinbad_.at(5)->setLoop(true);
+    }
+    
+    return false;
+}
+
+void Simbad::frameRendered(const Ogre::FrameEvent& evento)
+{
+    // Permite a la animacion detectar el paso del tiempo
+    
+    for (int i = 0; i < animationsSinbad_.size(); ++i)
+    {
+        animationsSinbad_.at(i)->addTime(evento.timeSinceLastFrame);
+        
+    }
+}
+
+void Simbad::receiveEvent(EntidadIG* entidad, string mensaje)
+{
+}
+
+void Simbad::StopAnim() {   // Resetea todas las animaciones
+    for (int i = 0; i < animationsSinbad_.size(); ++i)
+    {
+        animationsSinbad_.at(i)->setEnabled(false);
+        animationsSinbad_.at(i)->setLoop(false);
+    }
+}
--- ProyectosOGREvc15x86/IG2App/Simbad.h
+++ ProyectosOGREvc15x86/IG2App/Simbad.h
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <vector>
+using namespace Ogre;
 class Simbad : public EntidadIG
 {
 public:
@@ -29,16 +30,8 @@ public:
 		Simbad_->setPosition(-400, 100, 300);
 		Simbad_->setScale(20, 20, 20);
 
-		AnimationStateSet* aux = ent->getAllAnimationStates();
-		auto it = aux->getAnimationStateIterator().begin();
-		while (it != aux->getAnimationStateIterator().end())
-		{
-			auto s = it->first; 
-			cout << s << endl;
-			++it;
-		}
-		////////////////////////////////////
-		/*Ogre::AnimationStateSet* aux = Simbad_->getAllAnimationStates();
+		
+		Ogre::AnimationStateSet* aux = ent->getAllAnimationStates();
 		auto it = aux->getAnimationStateIterator().begin();
 
 		int cont = 0;
@@ -46,39 +39,73 @@ public:
 			auto s = it->first;
 			++it;
 			std::cout << " Sinbad Animation " << cont << ": " << s << "\n";
-			animationsSinbad_.push_back(Simbad_->getAnimationState(s));
+			animationsSinbad_.push_back(ent->getAnimationState(s));
 			++cont;
 		}
-		/*
-		auto skeleton = Simbad_->getSkeleton();
+
+		auto skeleton = ent->getSkeleton();
 		auto numBones = skeleton->getNumBones();
 		for (cont = 0; cont < numBones; cont++) {
 			std::cout << " Sinbad Bone " << cont << ": " << skeleton->getBone(cont)->getName() << "\n";
-		}*/
-		////////////////////////////////////////
+		}
+
+		animationsSinbad_.at(0)->setEnabled(anim);
+		animationsSinbad_.at(0)->setLoop(anim);
+
+		// ESPADAS
+		// Espada 1
+		sword1_ = mSM->createEntity("Sword.mesh");
+		sword1Node = Simbad_->createChildSceneNode("sword1");
+		
+		// Espada 2
+		sword2_ = mSM->createEntity("Sword.mesh");
+		sword2Node = Simbad_->createChildSceneNode("sword2");		
+
+		ent->attachObjectToBone("Sheath.L", sword1_);
+		ent->attachObjectToBone("Sheath.R", sword2_);
+
 	}
 	bool keyPressed(const OgreBites::KeyboardEvent& evento);
 	void frameRendered(const Ogre::FrameEvent& evento);
 	void receiveEvent(EntidadIG* entidad,string mensaje);
+
 protected:
+	void StopAnim();
 	Ogre::SceneNode* Simbad_;
-	/// 
-	std::vector<Ogre::AnimationState*> animationsSinbad_;
+	bool anim = true;
+	std::vector<AnimationState*> animationsSinbad_;
 	Ogre::Entity* ent;
+	Ogre::Entity* sword1_ = nullptr;
+	Ogre::Entity* sword2_ = nullptr;
+
 	Ogre::SceneNode* sword1Node = nullptr;
 	Ogre::SceneNode* sword2Node = nullptr;
 
 	// Animation 1
 	Ogre::AnimationState* animationState;
+
 	Ogre::NodeAnimationTrack* track;
 	Ogre::Animation* animation;
 
 	Ogre::Vector3 keyframePos;
 	Ogre::Vector3 desplazamiento;
-
 	Ogre::Vector3 orientation;
 	Ogre::Vector3 destino;
 
 	const Ogre::Real duration = 20;
+
 };
 
+	/*Dance			0
+	DrawSwords
+	HandsClosed
+	HandsRelaxed
+	IdleBase
+	IdleTop			5
+	JumpEnd
+	JumpLoop
+	JumpStart
+	RunBase
+	RunTop			10
+	SliceHorizontal
+	SliceVertical*/
