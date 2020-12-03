472fa93326f089527a7e12c3270c4364316fc974
Sergio Alberto Luis Cano <serluis@ucm.es>
2020-12-03 21:05:55 +0100

    intento de zoom animado

--- ProyectosOGREvc15x86/media/IG2App/SpaceVS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/SpaceVS.glsl
@@ -5,11 +5,17 @@
 
 in vec4 vertex; // atributos de los vértices a procesar
 in vec2 uv0; // coordenadas de textura 0
-uniform mat4 modelViewProjMat; // constante de programa
-out vec2 vUv0; // out del vertex shader
 
+uniform mat4 modelViewProjMat; // constante de programa
+uniform float SinTiempo;
+uniform float ZF;
+out vec2 vUv0; // out del vertex shader
+out vec2 vUv1; // otro out de vertexSh
 void main() {
 	vUv0 = uv0; // se pasan las coordenadas de textura
+	//vUv1 = ((uv0-0.5)*ZF) + 0.5; //Zoom fijo
+	
+	vUv1 = (uv0-0.5) * SinTiempo + 0.5; //Zoom animado
 	gl_Position = modelViewProjMat * vertex; //obligatorio
 	// (Clipping coordinates)
 }
