0dae0efc5831791e463fcff9c241efd43b1f78df
Jorzuiz <jordazurdo@hotmail.com>
2020-12-16 00:03:19 +0100

    Iluminacion cambiada

--- ProyectosOGREvc15x86/media/IG2App/CorrosionFS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/CorrosionFS.glsl
@@ -6,27 +6,30 @@
 uniform sampler2D texturaCorrosion; // tipo sampler2D para texturas 2D
 uniform sampler2D texturaBumpy;
 
-// luz ambiente uniform vec3 luzDiff; uniform vec3 luzDir;
-uniform vec4 InColor;	// Colores de la parte interior
-uniform vec4 OutColor; 	// Colores de la parte exterior
 uniform float Flipping;
+
+in vec2 vUv0; 			// out del vertex shader
+// luz ambiente uniform vec3 luzDiff; uniform vec3 luzDir;
+in vec3 vFrontColor; 	// Colores de la parte exterior (Difusa Normal aplicada)
+in vec3 vBackColor;		// Colores de la parte interior (Difusa -Normal aplicada)
+in vec3 viewNormal;
+in vec3 viewVertex; 	//v_position
+
 // Luces
-uniform vec3 luzUnicaPosition; //u_lightpos
-uniform vec3 luzUnicaDifusa;
+//uniform vec3 luzUnicaPosition; //u_lightpos
+//uniform vec3 luzUnicaDifusa;
 //in vec3 vFrontColor; // color de la iluminación interpolado
 //in vec3 vBackColor; // color de la iluminación interpolado
 
-in vec3 vXxxNormal;
-in vec3 vXxxVertex; //v_position
-in vec2 vUv0; 			// out del vertex shader
 out vec4 fFragColor; 	// out del fragment shader
+//out vec3 normal;
 
 //uniform sampler2D materialTex; // Front = Back
 
 void main() {
 
 	vec4 color;	// Color final a representar
-	vec3 normal; // normales finales direccion de refraccion de la luz
+	//vec3 normal; // normales finales direccion de refraccion de la luz
 	//Colores de las texturas, una es la transparencia, la otra se representa	
 	vec3 corrosion = texture(texturaCorrosion, vUv0).rgb; // Transforma a vec3
 	vec3 metal = texture(texturaBumpy, vUv0).rgb;		  // Transforma a vec3
@@ -38,21 +41,21 @@ void main() {
 
 	// Usa un color u otro en base a la parte que estemos mirando
 	if (frontFacing) {
-		color = vec4(metal, 1.0);//OutColor *  
-		normal = vXxxNormal; 
+		color = vec4(vFrontColor, 1.0) * vec4(metal, 1.0);  
+		//normal = viewNormal; 
 	}
 	
 	else {
-		color = InColor * vec4(1.0, 1.0, 0.0 , 1.0);
-		normal = -vXxxNormal;
+		color = vec4(vBackColor, 1.0) * vec4(1.0, 1.0, 0.0, 1.0);
+		//normal = -viewNormal;
 	}
 	
     //vec3 diffuse = diff * luzUnicaDifusa;
      
     //vec4 result = vec4(diffuse, 1.0);    
-	vec3 vectorlight = normalize(luzUnicaPosition - vXxxVertex);
-	float diff = max(dot(normal, vectorlight), 0.1);
+	//vec3 vectorlight = normalize(luzUnicaPosition - viewVertex);
+	//float diff = max(dot(normal, vectorlight), 0.1);
 
     // salida
-    fFragColor = color* diff;//vec4(result * color);
+    fFragColor = color;// * diff;//vec4(result * color);
 }
