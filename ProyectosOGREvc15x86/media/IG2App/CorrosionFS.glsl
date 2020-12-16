72b77526db74b163d355e333fdc747a109e7bea1
serluis <serluis@ucm.es>
2020-12-16 13:15:56 +0100

    se supone bien ya veremos

--- ProyectosOGREvc15x86/media/IG2App/CorrosionFS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/CorrosionFS.glsl
@@ -10,12 +10,20 @@ uniform float Flipping;
 
 in vec2 vUv0; 			// out del vertex shader
 // luz ambiente uniform vec3 luzDiff; uniform vec3 luzDir;
-in vec3 vFrontColor; 	// Colores de la parte exterior (Difusa Normal aplicada)
-in vec3 vBackColor;		// Colores de la parte interior (Difusa -Normal aplicada)
+
 in vec3 viewNormal;
 in vec3 viewVertex; 	//v_position
-
+uniform vec3 vFrontColor; 	// color rgb de la iluminación de la cara Front (normal)
+uniform vec3 vBackColor; 	// color rgb de la iluminación de la cara Back (-normal)
 // Luces
+// Variables para la iluminación
+//uniform vec3 lightAmbient; 		// intensidades de la luz
+uniform vec3 lightDiffuse;
+uniform vec3 materialDiffuse; 	// datos del material ¡Front=Back!
+
+uniform vec4 lightPosition; 	// datos de la fuente de luz en view space
+								// lightPosition.w == 0 -> Luz Direccional
+								// lightPosition.w == 1 -> Luz Posicional
 //uniform vec3 luzUnicaPosition; //u_lightpos
 //uniform vec3 luzUnicaDifusa;
 //in vec3 vFrontColor; // color de la iluminación interpolado
@@ -25,7 +33,13 @@ out vec4 fFragColor; 	// out del fragment shader
 //out vec3 normal;
 
 //uniform sampler2D materialTex; // Front = Back
-
+float diff(vec3 cVertex, vec3 cNormal)
+{
+	vec3 lightDir = lightPosition.xyz;
+	if(lightPosition.w == 1) {lightDir = (lightPosition.xyz - cVertex);}
+	return max(dot(cNormal, normalize(lightDir)), 0.0);
+	// dot: coseno ángulo
+}
 void main() {
 
 	vec4 color;	// Color final a representar
@@ -34,6 +48,13 @@ void main() {
 	vec3 corrosion = texture(texturaCorrosion, vUv0).rgb; // Transforma a vec3
 	vec3 metal = texture(texturaBumpy, vUv0).rgb;		  // Transforma a vec3
 
+	//vec3 ambient = lightAmbient * materialDiffuse;
+	vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * materialDiffuse;
+	vec3 colorF = diffuse; // + specular + ambient
+
+	diffuse =  diff(viewVertex, -viewNormal) * lightDiffuse * materialDiffuse;//
+	vec3 colorB =  diffuse; // + specular ambient +
+    //vec3 diffuse = diff * luzUnicaDifusa;
 	bool frontFacing = (Flipping > -1)? gl_FrontFacing : ! gl_FrontFacing;
 	// Color que queremos esliminar para hacerlo transparente
 	// En este caso es cualquier rojo sobre 0.6
@@ -41,16 +62,15 @@ void main() {
 
 	// Usa un color u otro en base a la parte que estemos mirando
 	if (frontFacing) {
-		color = vec4(vFrontColor, 1.0) * vec4(metal, 1.0);  
+		color = vec4(colorF, 1.0) * vec4(metal, 1.0) * vec4(vFrontColor, 1.0);  
 		//normal = viewNormal; 
 	}
 	
 	else {
-		color = vec4(vBackColor, 1.0) * vec4(1.0, 1.0, 0.0, 1.0);
+		color = vec4(colorB, 1.0) * vec4(vBackColor, 1.0);
 		//normal = -viewNormal;
 	}
 	
-    //vec3 diffuse = diff * luzUnicaDifusa;
      
     //vec4 result = vec4(diffuse, 1.0);    
 	//vec3 vectorlight = normalize(luzUnicaPosition - viewVertex);
--- ProyectosOGREvc15x86/media/IG2App/CorrosionVS.glsl
+++ ProyectosOGREvc15x86/media/IG2App/CorrosionVS.glsl
@@ -6,15 +6,6 @@ in vec4 vertex; // atributos de los vértices a procesar
 in vec3 normal;	// Normal del plano que compone la cara
 in vec2 uv0; // coordenadas de textura 0
 
-// Variables para la iluminación
-uniform vec3 lightAmbient; 		// intensidades de la luz
-uniform vec3 lightDiffuse;
-uniform vec3 materialDiffuse; 	// datos del material ¡Front=Back!
-
-uniform vec4 lightPosition; 	// datos de la fuente de luz en view space
-								// lightPosition.w == 0 -> Luz Direccional
-								// lightPosition.w == 1 -> Luz Posicional
-
 // Matrices para Transformaciones de espacio de coordenadas
 uniform mat4 modelViewMat; // View*Model matrix
 uniform mat4 modelViewProjMat;  // constante de programa
@@ -22,22 +13,15 @@ uniform mat4 normalMat; 		// = transpose(inverse(modelView))
 
 // Valores para el Fragment Shader
 out vec2 vUv0; 			// coordenadas de textura
-out vec3 vFrontColor; 	// color rgb de la iluminación de la cara Front (normal)
-out vec3 vBackColor; 	// color rgb de la iluminación de la cara Back (-normal)
+
 out vec3 viewNormal; 	// coordenadas de la normal en  Xxx space
 out vec3 viewVertex; 	// coordenadas de el vértice en Xxx space
 
-float diff(vec3 cVertex, vec3 cNormal)
-{
-	vec3 lightDir=lightPosition.xyz;
-	if(lightPosition.w==1){lightDir=(lightPosition.xyz-cVertex);}
-	return max(dot(cNormal, normalize(lightDir)), 0.0);
-	// dot: coseno ángulo
-}
+
 
 void main() {
 	// Luz Ambiente
-	vec3 ambient = lightAmbient * materialDiffuse;
+	
 	
 	// valores en espacio de vista para el FS
 	vec3 viewVertex = vec3(modelViewMat * vertex);					// Coordenadas para el FS
@@ -45,10 +29,7 @@ void main() {
 
 	// Luz difusa		
 	// Calculos de iluminacion
-	vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * materialDiffuse;
-	vFrontColor = ambient + diffuse; // + specular
-	diffuse=diff(viewVertex,-viewNormal)*lightDiffuse*materialDiffuse;
-	vBackColor = ambient + diffuse; // + specular
+	
 
 	vUv0 = uv0; 										// se pasan las coordenadas de textura
 	gl_Position = modelViewProjMat * vertex;	//, 0.0); 		// Posicion del Fragmento; La matriz determina el espacio de coordenadas
